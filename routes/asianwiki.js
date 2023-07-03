f7e8561bf06bf38bfaaa816d6a7cefd75704c431
jabirdev <cs.jabirdev@gmail.com>
2023-07-03 17:02:54 +0700

    add log

--- routes/asianwiki.js
+++ routes/asianwiki.js
@@ -66,8 +66,8 @@ const getMainPage = async (req, res) => {
 
     const response = await gotScraping.get(url)
     const $ = cheerio.load(response.body)
-    console.log(response.body)
     const featuredElement = $('#amazingslider-1 ul li')
+    console.log(featuredElement.toString())
     const moviesElement = $('#slidorion #slider .slide')
     const dramasElement = $('#slidorion2 #slider .slide2')
 
